84f853c3d7d7afadb95e6c9751880d61bc8f5844
WodaMineralna <raknieborak65@gmail.com>
2025-10-17 20:37:37 +0200

    feat(utils): add customizable logger util with colored log levels and caller metadata
    - Created `utils/logger.js` with support for log levels: info, success, warn, error
    - Each log displays:
      - relative file path (based on import.meta.url)
      - line number of caller
      - function name
    - Added colorized output using `chalk` for better readability
    - Each log automatically includes caller info (file, line, function name)
    - Replaced raw console.log usage with a consistent and colorized logger interface

---
+++ utils/logger.js
@@ -0,0 +1,64 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import chalk from "chalk";
+
+const projectRoot = process.cwd();
+
+const colorPallete = {
+  info: chalk.blue,
+  success: chalk.green,
+  warn: chalk.yellow,
+  error: chalk.red,
+};
+
+function createLogger(metaUrl) {
+  const filePath = path.relative(projectRoot, fileURLToPath(metaUrl));
+  return (...args) => {
+    const { line, fn } = getCallerInfo();
+    log(...args, `${filePath}:${line}`, fn);
+  };
+}
+
+function getCallerInfo() {
+  const err = new Error();
+  const stack = err.stack?.split("\n");
+
+  if (!stack || stack.length < 4) return { line: "?", fn: "unknown" };
+
+  const lineMatch = stack[3].match(/:(\d+):\d+\)?$/);
+  let fnMatch = stack[3].match(/at (\S+)/);
+
+  // handles cases where fnMatch is an absolute URL path, not e.g. 'at productSchema.statics.addProduct'
+  let match;
+  if (fnMatch[1].includes("///")) {
+    match = fnMatch[1].match(/file:\/\/.*\/([^/]+)\.js:\d+:\d+$/);
+  }
+
+  fnMatch = match ?? fnMatch;
+
+  return {
+    line: lineMatch ? lineMatch[1] : "?",
+    fn: fnMatch ? fnMatch[1] : "unknown",
+  };
+}
+
+function log(
+  errorType,
+  message = "Log message not defined",
+  filePath = "?",
+  fnName = "unknown"
+) {
+  if (!Object.keys(colorPallete).includes(errorType))
+    return console.log(
+      colorPallete.error(
+        `\nInvalid logger 'errorType' (${errorType}) in filePath ${filePath}`
+      )
+    );
+
+  return console.log(
+    chalk.dim(`[${filePath}]`, `[${fnName}]`),
+    colorPallete[errorType](`${message}`)
+  );
+}
+
+export default createLogger;
