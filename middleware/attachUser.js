2f924931919e56a6ff94ed84939afd6b166fa7f7
WodaMineralna <raknieborak65@gmail.com>
2025-10-18 18:43:03 +0200

    fix(session): handle edge cases in `attachUser`
    - Previously, under certain circumstances an unhandled error was thrown during unauthenticated try to access protected route
    - Now, all cases are handled appropriately
    
    - Also integrated custom `logger` to `attachUser`

--- middleware/attachUser.js
+++ middleware/attachUser.js
@@ -1,11 +1,27 @@
 import User from "../models/user.js";
 import catchErrAsync from "../utils/catchErrAsync.js";
 
+import createLogger from "../utils/logger.js";
+
+const log = createLogger(import.meta.url);
+
 export default catchErrAsync(async (req, res, next) => {
-  if (!req.session.user) return next();
+  if (!req.session.user) {
+    log("warn", "No user session found");
+    return next();
+  }
 
   let user = await User.findById(req.session.user._id);
-  req.user = user;
+  if (!user) {
+    log(
+      "warn",
+      "User session found, but user does not match any user in database"
+    );
+    return next();
+  }
 
-  next();
+  req.user = user;
+  // log("info", "User attached to session");
+
+  return next();
 });
